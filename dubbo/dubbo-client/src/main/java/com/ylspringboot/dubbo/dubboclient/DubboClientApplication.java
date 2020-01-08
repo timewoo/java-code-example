c502de391f5da8cdd99d63b8ac011081a0bcaa4e
yanglin <1179595024@qq.com>
2020-01-08 14:58:26 +0800

    dubbo项目

---
+++ dubbo/dubbo-client/src/main/java/com/ylspringboot/dubbo/dubboclient/DubboClientApplication.java
@@ -0,0 +1,16 @@
+package com.ylspringboot.dubbo.dubboclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author yanglin
+ * @date 2020/1/8
+ */
+@SpringBootApplication
+public class DubboClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DubboClientApplication.class, args);
+    }
+}
---
+++ dubbo/dubbo-server/src/main/java/com/ylspringboot/dubbo/dubboserver/DubboServerApplication.java
@@ -0,0 +1,16 @@
+package com.ylspringboot.dubbo.dubboserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author yanglin
+ * @date 2020/1/8
+ */
+@SpringBootApplication
+public class DubboServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DubboServerApplication.class, args);
+    }
+}
